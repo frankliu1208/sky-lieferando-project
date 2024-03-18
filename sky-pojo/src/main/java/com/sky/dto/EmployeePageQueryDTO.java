2f528617c29cc02ee5000907cb44bde2afe871e4
liuhang6666 <liuhang@oregonstate.edu>
2024-03-18 11:55:17 +0100

    complete: employee pagination search feature

--- sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
@@ -4,16 +4,18 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+// pagination search feature:  below 3 parameters are values that frontend need to send to backend when making POST or GET request
+// Returned value from backend will be encapsulated into Result<PageResult> object
 @Data
 public class EmployeePageQueryDTO implements Serializable {
 
     //员工姓名
     private String name;
 
-    //页码
+    //页码  page number
     private int page;
 
-    //每页显示记录数
+    //每页显示记录数  items per page
     private int pageSize;
 
 }
