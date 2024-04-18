403351227a4f533b7869c6e50d5bb9bb0301a4fd
liuhang6666 <liuhang@oregonstate.edu>
2024-04-18 17:27:03 +0200

    finish: file upload feature

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,13 +1,18 @@
 package com.sky.controller.admin;
 
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/admin/common")
@@ -15,11 +20,27 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 public class CommonController {
 
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
     // the argument "file" need to be aligned with the "key" name in the request body from the frontend request
     @PostMapping("/upload")
     @ApiOperation("file upload")
     public Result<String> upload(MultipartFile file) {
         log.info("file: {}", file);
+
+        try {
+
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + extension;
+
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("file is not uploaded successfully", e );
+        }
+
         return null;
     }
 
